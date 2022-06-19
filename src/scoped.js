b2a7afb80b3b643f5410a99349da57d34caf1174
github.arief <78076941+arief-github@users.noreply.github.com>
2022-06-19 09:47:12 +0700

    add file js for scoped materials

---
+++ src/scoped.js
@@ -0,0 +1,41 @@
+// Let and Const
+// Let and Const
+// let : sifatnya sama seperti var. namun let mendukung block scoped ketimbang var yang selalu global scoped
+
+// let
+let x = 2;
+x += 40;
+console.log(x);
+
+// const: sifat dari variable const ini yaitu satu kali assign. kita tidak dapat merubah nilainya kembali
+// ataupun di deklarasi ulang
+
+const value = Math.random();
+// const value = 1;
+console.log(value < 0.5 ? "Heads" : "Tails");
+
+
+// True block scoped
+// kita lihat penggunaan var dalam block scoped
+function jumpOut() {
+    var a = [1, 2, 3];
+    for (var i = 0; i < a.length; ++i) {
+        var value = a[i];
+        console.log(value);
+    }
+
+    console.log("Outside Loop" + value); // var bisa akses nilai value disini.
+}
+
+jumpOut();
+
+//  kita coba menggunakan let/const. saya pakai let. let dan const punya blocked scoped.
+function stayContained() {
+    var a = [1, 2, 3];
+    for (var i = 0; i < a.length; ++i) {
+        let value = a[i];
+        console.log(value);
+    }
+    console.log("Outside loop" + value); // ReferenceError: 'value' is not defined
+}
+stayContained();
